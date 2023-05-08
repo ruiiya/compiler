2cb6d80846d895d2d852b6d1fcf1c81e43653924
ruiiya <58024483+ruiiya@users.noreply.github.com>
2023-05-08 08:39:11 +0700

    fix read number lexer

--- lexer.h
+++ lexer.h
@@ -61,32 +61,77 @@ namespace xd
 
                 if(find(terminals.begin(), terminals.end(), identifier_string) != terminals.end()) {
                     return token(identifier_string, identifier_string);
+                } else if(identifier_string == "true" || identifier_string == "false") {
+                    return token("boolean_constant", identifier_string);
                 } else {
                     return token("identifier", identifier_string);
                 }
             }
 
-            if(isdigit(last_char)) {
+            if(isdigit(last_char) || last_char == '.') {
                 string num_string;
-                num_string += last_char;
-                while(ifs.get(last_char) && isalnum(last_char)) {
+
+                bool has_dot = false;
+                bool has_e = false;
+                bool has_sign = false;
+                bool has_digits_before_dot = false;
+                bool has_digits_after_dot = false;
+                bool has_digits_after_e = false;
+
+                bool is_error = false;
+
+                do {
+                    if (last_char == '.') {
+                        if (has_dot || has_e) {
+                            is_error = true;
+                            break;
+                        }
+                        has_dot = true;
+                    } else if (last_char == 'e' || last_char == 'E') {
+                        if (has_e) {
+                            is_error = true;
+                            break;
+                        }
+                        has_e = true;
+                    } else if (isdigit(last_char)) {
+                        if (has_e) {
+                            has_digits_after_e = true;
+                        } else if (has_dot) {
+                            has_digits_after_dot = true;
+                        } else {
+                            has_digits_before_dot = true;
+                        }
+                    } else if(isalpha(last_char)) {
+                        is_error = true;
+                        break;
+                    } else {
+                        break;
+                    }
                     num_string += last_char;
+                }while(!ifs.eof() && ifs.get(last_char));
+
+                if(isalpha(last_char)) {
+                    num_string += last_char;
+                    while(ifs.get(last_char) && isalnum(last_char)) {
+                        num_string += last_char;
+                    }
                 }
 
                 if (ifs.eof()) {
                     last_char = ' ';
                 }
 
-                if(isFloat(num_string)) {
-                    for(char& c : num_string) {
+                if(is_error) {
+                    error = "Wrong fractional number format : " + num_string;
+                    return token("__ERROR__", "__ERROR__");
+                }
+
+                for(char& c : num_string) {
                         if(!isdigit(c)) {
                             return token("float_constant", num_string);
                         }
                     }
-                    return token("integer_constant", num_string);
-                } 
-                error = "Wrong fractional number format : " + num_string;
-                return token("__ERROR__", "__ERROR__");
+                return token("integer_constant", num_string);
             }
 
             if(ispunct(last_char)) {
@@ -151,6 +196,7 @@ namespace xd
                         }
 
                         literal_string.pop_back();
+                        ifs.get(last_char);
                         tok = token("string_constant", '"' + literal_string + '"');
                         break;
 
